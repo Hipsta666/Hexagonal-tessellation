3bacef7127ab608072df0c2f99b22656d7e0d091
hipstaa666 <andew-hipsta@mail.ru>
2018-11-30 01:22:32 +0700

    version 1.1

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from turtle import *
 
-
 # The function that draws a regular hexagon based on the width.
 def hexagon(x, y, size_length, number, light_1, light_2):
     up()
@@ -12,7 +11,7 @@ def hexagon(x, y, size_length, number, light_1, light_2):
     for count in range(number):
         if count % 2 == 0:
             begin_fill()
-            color("black", light_1)
+            color("black", light_2)
             for _ in range(6):
                 forward(size_length)
                 left(60)
@@ -26,7 +25,7 @@ def hexagon(x, y, size_length, number, light_1, light_2):
             end_fill()
         else:
             begin_fill()
-            color("black", light_2)
+            color("black", light_1)
             for _ in range(6):
                 forward(size_length)
                 left(60)
@@ -44,25 +43,34 @@ def hexagon(x, y, size_length, number, light_1, light_2):
 
 # The function that performs surface tiling with hexagons.
 # Further function
-number_of_hexagons = int(input())
-side_length = (500 / number_of_hexagons) / (3 ** (1 / 2))
-for p in range(1, number_of_hexagons + 1):
-    if p == 3 or p == 7 or p == 11 or p == 15 or p == 19:
-        hexagon(-250, 250 - 1.5 * side_length * p, side_length,
-                number_of_hexagons, "green", "red")
-    elif p == 4 or p == 8 or p == 12 or p == 16 or p == 20:
-        hexagon(-250 - ((3 ** (1 / 2)) * side_length) / 2,
-                250 - 1.5 * side_length * p, side_length,
-                number_of_hexagons, "green", "red")
-    elif p % 2 != 0:
-        hexagon(-250, 250 - 1.5 * side_length * p,
-                side_length, number_of_hexagons, "red", "green")
-    else:
-        hexagon(-250 - ((3 ** (1 / 2)) * side_length) / 2,
-                250 - 1.5 * side_length * p, side_length,
-                number_of_hexagons, "red", "green")
-mainloop()
+def mosaic(number, length, color_one, color_two):
+    for p in range(1, number + 1):
+        if p == 3 or p == 7 or p == 11 or p == 15 or p == 19:
+            hexagon(-250, 250 - 1.5 * length * p, length,
+                    number, color_one, color_two)
+        elif p == 4 or p == 8 or p == 12 or p == 16 or p == 20:
+            hexagon(-250 - ((3 ** (1 / 2)) * length) / 2,
+                    250 - 1.5 * length * p, length,
+                    number, color_one, color_two)
+        elif p % 2 != 0:
+            hexagon(-250, 250 - 1.5 * length * p,
+                    length, number, color_two, color_one)
+        else:
+            hexagon(-250 - ((3 ** (1 / 2)) * length) / 2,
+                    250 - 1.5 * length * p, length,
+                    number, color_two, color_one)
+
+
+# The function that adapts the name of the text for the program.
 
 
 # The function that "talks to the user".
 
+
+# Test
+first_color = str(input("Введите первый цвет:"))
+second_color = str(input("Введите второй цвет:"))
+number_of_hexagons = int(input())
+side_length = (500 / number_of_hexagons) / (3 ** (1 / 2))
+mosaic(number_of_hexagons, side_length, first_color, second_color)
+mainloop()
